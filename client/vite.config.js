584f00db157fdcef027d93f6c5166ccee4e82678
nikhilchavan2004 <nikhilchavan4002@gmail.com>
2024-11-24 16:07:35 +0530

    n

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig({
   server: {
     proxy: {
       '/api': {
@@ -15,4 +11,5 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}));
+  plugins: [react()],
+});
