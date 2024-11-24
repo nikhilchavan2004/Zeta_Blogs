0f2abe3becc97340968c258ba05f960bd6bff9a2
nikhilchavan2004 <nikhilchavan4002@gmail.com>
2024-11-24 15:42:30 +0530

    change

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,15 +15,4 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-  plugins: [react()],
-});
+}));
